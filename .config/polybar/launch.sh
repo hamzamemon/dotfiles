ec4c10acf42254570c0bc2750970d5b43cacef29
Hamza Memon <hmemon67@gmail.com>
2020-04-21 22:51:30 -0700

    Moved Scripts folder, updated polybar config, updated maim folder location.

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 #!/usr/bin/env sh
 
 # Terminate already running bar instances
@@ -7,8 +6,12 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-polybar --reload left &
-polybar --reload center &
-polybar --reload right &
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload default &
+  done
+else
+  polybar --reload default &
+fi
 
 echo "Bars launched..."
--- .zprofile
+++ .zprofile
@@ -1,4 +1,9 @@
-# Profile file. Runs on login. Environmental variables set here.
+#!/bin/zsh
+
+# zsh profile file. Runs on login. Environmental variables set here.
+
+# Adds `~/.local/bin` to $PATH
+export PATH="$PATH:$(du "$HOME/.local/bin/" | cut -f2 | tr '\n' ':' | sed 's/:*$//')"
 
 # Default programs
 export EDITOR="nvim"
@@ -20,4 +25,3 @@ LESS_TERMCAP_se=$'\E[0m' \
 LESS_TERMCAP_so=$'\E[38;5;246m' \
 LESS_TERMCAP_ue=$'\E[0m' \
 LESS_TERMCAP_us=$'\E[04;38;5;146m' \
-
